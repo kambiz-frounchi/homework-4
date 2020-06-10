d4a195bc69a634c57a039a2c5b3f88ebb4d2084a
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-06-10 17:09:16 -0400

    various fixes

--- script.js
+++ script.js
@@ -1,53 +1,41 @@
-/*GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score*/
-
+//constants
 var INTERVAL = 1;
-var MAX_TIME = 20;
+var MAX_TIME = 60;
 var TIME_DECREMENT_IF_WRONG = 10;
-var question = 0;
-var timerHandle = null;
 
 var questionAnswer1 = {
-    question: "question 1?",
-    answers: ["answer11", "answer12", "answer13", "answer14"],
-    correctAnswer: 0
+    question: "Which choice is not a primitive type in JavaScript?",
+    answers: ["Number", "Object", "String", "Boolean"],
+    correctAnswer: 1
 }
 
 var questionAnswer2 = {
-    question: "question 2?",
-    answers: ["answer21", "answer22", "answer23", "answer24"],
+    question: "Which of the below functions is not used with DOM elements?",
+    answers: ["push", "querySelector", "createElement", "removeChild"],
     correctAnswer: 0
 }
 
 var questionAnswer3 = {
-    question: "question 3?",
-    answers: ["answer31", "answer32", "answer33", "answer34"],
-    correctAnswer: 0
+    question: "What keyword is used to define a variable?",
+    answers: ["for", "function", "if", "var"],
+    correctAnswer: 3
 }
 
 var questionAnswer4 = {
-    question: "question 4?",
-    answers: ["answer41", "answer42", "answer43", "answer44"],
-    correctAnswer: 0
+    question: "Is it required to explicitly define the type of a variable in JavaScript?",
+    answers: ["depends", "no", "yes", "N/A"],
+    correctAnswer: 1
 }
 
 var questionAnswer5 = {
-    question: "question 5?",
-    answers: ["answer51", "answer52", "answer53", "answer54"],
-    correctAnswer: 0
+    question: "what symbol typically defines the start of a variable scope in Javascript?",
+    answers: ["[", "tab", "{", "="],
+    correctAnswer: 2
 }
 
 questionAnswers = [questionAnswer1, questionAnswer2, questionAnswer3, questionAnswer4, questionAnswer5];
 
+//quiz class
 var quiz = {
     questionAnswers: [],
     maxTime: 60, //60 seconds
@@ -60,10 +48,15 @@ var quiz = {
     scoreIncrement: 20,
     highScores: [],
     initialize: function (interval, maxTime, timeDecrementIfWrong, questionAnswers) {
+        this.timePassed = 0;
+        this.currentQuestionNumber = 0;
+        this.score = 0;
+        this.highScores = [];
         this.interval = interval;
         this.maxTime = maxTime;
         this.timeDecrementIfWrong =  timeDecrementIfWrong;
         this.numQuestions = questionAnswers.length;
+        this.questionAnswers = [];
         questionAnswers.forEach(element => {
             this.questionAnswers.push(element);
         });
@@ -79,6 +72,11 @@ var quiz = {
     }
 }
 
+//HTML elements
+var highlightsPage = document.querySelector("#highlights-page");
+var resultsPage = document.querySelector("#results-page");
+var startPage = document.querySelector("#start-page");
+var questionPage = document.querySelector("#question-page");
 var bodyElement = document.querySelector("body");
 var startButtonElement = document.querySelector("#start-button");
 var questionAnswerElement = document.querySelector("#question-answer");
@@ -89,16 +87,18 @@ var buttonElement = null;
 var submitElement = document.querySelector("#submit-button");
 var clearHighlightsElement = document.querySelector("#clear-highlights");
 var startOverElement = document.querySelector("#start-over");
-var highlightsPage = document.querySelector("#highlights-page");
-var resultsPage = document.querySelector("#results-page");
-var startPage = document.querySelector("#start-page");
+var timerElement = document.querySelector("#timer");
 
+//other global variables
+var timerHandle = null;
+
+//functions
 function clearHighlights() {
     console.log("clearHighlights");
     var highScoresElement = document.querySelector("#high-scores");
-    var parentElement = highScoresElement.parentElement;
-    //parentElement.removeChild(highScoresElement);
-    highScoresElement.classList.add("invisible");
+    while (highScoresElement.firstChild) {
+        highScoresElement.removeChild(highScoresElement.firstChild);
+    }
     localStorage.removeItem("highScores");
 }
 
@@ -107,15 +107,16 @@ function showHighlights() {
     highlightsPage.classList.remove("invisible");
     var highScoresElement = document.querySelector("#high-scores");
     quiz.highScores.sort(function (a, b) {
-        return a.score - b.score;
+        return b.score - a.score; //descending
     });
 
+    console.log(quiz.highScores);
     quiz.highScores.forEach((highScore, index) => {
         var scoreElement = document.createElement("h3");
         var rank = index + 1;
-        scoreElement.textContent = rank + "." + highScore.initials + " " + highScore.score;
+        scoreElement.textContent = rank + ". " + highScore.initials + "     " + highScore.score;
         console.log(scoreElement.textContent);
-        highScoresElement.prepend(scoreElement);
+        highScoresElement.appendChild(scoreElement);
     });
 }
 
@@ -125,18 +126,12 @@ function submitButtonCb() {
     var highScore = {initials: initialsElement.value, score: quiz.score};
     quiz.highScores.push(highScore);
     localStorage.setItem("highScores", JSON.stringify(quiz.highScores));
-    var parentElement = resultsPage.parentElement;
-    //parentElement.removeChild(resultsPage);
     resultsPage.classList.add("invisible");
     showHighlights();
 }
 
 function showResults() {
     console.log("show results");
-    var questionPage = document.querySelector("#question-page");
-    //questionPage.classList.add("invisible");
-    var parentElement = questionPage.parentElement;
-    //parentElement.removeChild(questionPage);
     questionPage.classList.add("invisible");
     var finalScore = document.querySelector("#final-score");
     finalScore.textContent = quiz.score;
@@ -178,12 +173,12 @@ function answerButtonCb(event) {
 }
 
 function createQuestionElements(questionNumber) {
+    console.log("createQuestionElements" + questionNumber);
     questionElement.textContent = questionAnswers[questionNumber].question;
     quiz.questionAnswers[questionNumber].answers.forEach(function(answer, index) {
         buttonId = "button-" + index;
         console.log("#" + buttonId);
         buttonElement = document.querySelector("#" + buttonId);
-        buttonElement.classList.remove("invisible");
         buttonElement.setAttribute("class", "btn btn-secondary");
         buttonElement.textContent = answer;
         buttonElement.addEventListener("click", answerButtonCb);
@@ -196,27 +191,35 @@ function timerCb() {
     if (quiz.timePassed >= quiz.maxTime) {
         console.log("time out!");
         clearInterval(timerHandle);
+        showResults();
     }
     quiz.timePassed++;
-
+    var timeRemaining = quiz.maxTime - quiz.timePassed;
+    if (timeRemaining >= 0) {
+        timerElement.textContent = timeRemaining;
+    } 
 }
 
 function startButtonCb() {
     timerHandle = setInterval(timerCb, quiz.interval * 1000);
-    //var parentElement = startButtonElement.parentElement;
-    //parentElement.removeChild(startButtonElement);
     startPage.classList.add("invisible");
+    questionPage.classList.remove("invisible");
     createQuestionElements(0);
 }
 
 function startOver() {
-    highlightsPage.classList.add("invisible");
-    startPage.classList.remove("invisible");
     quiz.initialize(INTERVAL, MAX_TIME, TIME_DECREMENT_IF_WRONG, questionAnswers);
     var highScores = JSON.parse(localStorage.getItem("highScores"));
+    console.log(highScores);
     if (highScores) {
         quiz.populateHighScores(highScores);
     }
+    if (timerHandle) {
+        clearInterval(timerHandle);
+    }
+    timerElement.textContent = quiz.maxTime;
+    highlightsPage.classList.add("invisible");
+    startPage.classList.remove("invisible");
 }
 
 
